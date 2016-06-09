445410d801caa648a3f6b362e1d4d85f127cfd3c
MarcosMenegazzo <correiodomarcos@gmail.com>
2016-06-09 09:44:34 -0300

    Migrated from SVN without history SVN at Capesesp had some issues that prevented me from extracting all metadata

---
+++ docroot/_diffs/js/iniciar.js
@@ -0,0 +1,15 @@
+/**
+ * inicialização dos componentes
+ */
+$(document).ready(function(){
+	$(".chzn-select").chosen();
+	$("#umclique").chosen({allow_single_deselect: true});
+
+	$("#umclique").change(function(){
+		var serviceLink = $("#umclique").val();
+		window.open(serviceLink);
+	});
+
+	$("table").addClass("table table-striped");
+});
+
